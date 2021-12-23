2cf529268620083e80a3846335a502d25475026d
yuekcc <yuekcc@qq.com>
2021-12-24 00:52:46 +0800

    指定 http method

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import { logger } from './util/logger';
 import { updateIndexAsync, searchInEngine, checkHealth } from './indexmgr';
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1/status', async (req, res) => {
+app.get('/api/v1/status', async (req, res) => {
   const isEngineOk = await checkHealth();
   if (isEngineOk) {
     res.json({ message: 'ok' });
@@ -15,7 +17,7 @@ app.use('/api/v1/status', async (req, res) => {
   res.json({ error: true, message: 'engine is down' });
 });
 
-app.use('/api/v1/:site/search', async (req, res) => {
+app.get('/api/v1/:site/search', async (req, res) => {
   const indexName = req.params.site;
   const { keyword, pageIndex, pageSize } = req.query;
 
@@ -28,13 +30,19 @@ app.use('/api/v1/:site/search', async (req, res) => {
   res.status(400).json({ error: true, message: 'missing parameter' });
 });
 
-app.use('/api/v1/:site/hook', (req, res) => {
+app.post('/api/v1/:site/hook', (req, res) => {
   const indexName = req.params.site;
+  const signature = req.headers['X-Hub-Signature'.toLowerCase()];
+  const signature256 = req.headers['X-Hub-Signature-256'.toLowerCase()];
 
-  updateIndexAsync({ indexName, commitMessage: {} });
+  logger.info(`get hook callback, signature=${signature}, signature256=${signature256}`);
+  logger.info(`get hook callback data: ${JSON.stringify(req.body, null, 2)}`);
+
+  updateIndexAsync({ indexName, commitMessage: { id: req.body?.after } });
   res.json({ message: 'start-update-index' });
 });
 
 app.listen(CONFIG.port, () => {
   logger.info(`Host at http://localhost:${CONFIG.port}`);
+  logger.info(`Work dir: ${process.cwd()}`);
 });
